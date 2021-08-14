4ac605cbcd5503c659b459a9cf23f8122f801f53
kito0 <drewshah0@gmail.com>
2021-08-14 01:15:55 -0600

    Create entrypoint exporting components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './scss/main.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();
+export * from './components/Button';
